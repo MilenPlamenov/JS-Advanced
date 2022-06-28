65ec4efa0896906eec2387833797fe227fdd39ba
MilenPlamenov <milenkrasy@abv.bg>
2022-06-28 21:55:07 +0300

    Started working on Objects exercise

---
+++ Exercise - Objects & Composition/05. Lowest Prices in Cities.js
@@ -0,0 +1,26 @@
+function solve(arr) {
+    result = {};
+    for (el of arr) {
+        let [town, product, price] = el.split(' | ');
+        price = Number(price);
+        
+        if (!result[product]) {
+            result[product] = { town, price: price }
+        } else {
+            result[product] = result[product].price <= price ? result[product] : result[product] = { town, price: price }
+        }
+    }
+
+    for (const product in result) {
+        
+        console.log(`${product} -> ${result[product].price} (${result[product].town})`);
+   }
+}
+
+solve(['Sample Town | Sample Product | 1000',
+'Sample Town | Orange | 2',
+'Sample Town | Peach | 1',
+'Sofia | Orange | 3',
+'Sofia | Peach | 2',
+'New York | Sample Product | 1000.1',
+'New York | Burger | 10'])
